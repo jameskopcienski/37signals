be2bb9dbb7614649936e7459a86dcafb7381e35b
James Kopcienski <jkopcienski@gmail.com>
2013-05-29 10:13:02 -0400

    modify files

--- script.js
+++ script.js
@@ -2,41 +2,35 @@ $(document).ready(function() {
   
   $('#badgeleft').on('mouseenter', function() {
     event.preventDefault();
-    $('#holdheadlines').html($('#headlines').html());
     $('#headlines').html($('#leftheadlines').html());
     $('#headlines h1').addClass('h1small');
     $('#headlines h2').addClass('h2small');
     $('#headlines img').addClass('arrowleft');
   }).on('mouseleave', function(){
     event.preventDefault();
-    $('#headlines').html($('#holdheadlines').html());
-    $('#holdheadlines').html($('#leftheadlines').html());
-});
+    $('#headlines').html($('#mainheadlines').html());
+  });
 
   $('#badgeright').on('mouseenter', function() {
     event.preventDefault();
-    $('#holdheadlines').html($('#headlines').html());
     $('#headlines').html($('#rightheadlines').html());
     $('#headlines h1').addClass('h1small');
     $('#headlines h2').addClass('h2small');
     $('#headlines img').addClass('arrowright');
- }).on('mouseleave', function(){
+  }).on('mouseleave', function(){
     event.preventDefault();
-    $('#headlines').html($('#holdheadlines').html());
-    $('#holdheadlines').html($('#rightheadlines').html());
- });
+    $('#headlines').html($('#mainheadlines').html());
+  });
 
   $('#badgecenter').on('mouseenter', function() {
     event.preventDefault();
-    $('#holdheadlines').html($('#headlines').html());
     $('#headlines').html($('#centerheadlines').html());
     $('#headlines h1').addClass('h1small');
     $('#headlines h2').addClass('h2small');
     $('#headlines img').addClass('arrowcenter');
   }).on('mouseleave', function(){
     event.preventDefault();
-    $('#headlines').html($('#holdheadlines').html());
-    $('#holdheadlines').html($('#centerheadlines').html());
- });
+    $('#headlines').html($('#mainheadlines').html());
+  });
 
 });
